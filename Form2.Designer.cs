25cce1c5ff8bad024c6319ce0d892d10fb19ab8f
Anton Jesuthas Nickson Joram <46377224+iamnickson@users.noreply.github.com>
2019-08-05 15:26:48 +0530

    Add files via upload

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -39,6 +39,7 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button3 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
@@ -46,6 +47,7 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.button3);
             this.panel1.Controls.Add(this.textBox3);
             this.panel1.Controls.Add(this.textBox2);
             this.panel1.Controls.Add(this.textBox1);
@@ -82,7 +84,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(313, 221);
+            this.button2.Location = new System.Drawing.Point(216, 221);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(101, 38);
             this.button2.TabIndex = 6;
@@ -92,7 +94,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(138, 221);
+            this.button1.Location = new System.Drawing.Point(38, 221);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(101, 38);
             this.button1.TabIndex = 5;
@@ -149,6 +151,16 @@
             this.dataGridView1.Size = new System.Drawing.Size(423, 218);
             this.dataGridView1.TabIndex = 2;
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(386, 221);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(101, 38);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "UPDATE";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -181,5 +193,6 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button3;
     }
 }
--- Form2.cs
+++ Form2.cs
@@ -46,10 +46,66 @@ namespace DisplayTable
                 }
                 else
                 {
-                    System.Windows.Forms.MessageBox.Show("Updated Successfully");
+                    System.Windows.Forms.MessageBox.Show("Inserted Successfully");
                 }
                 mycon.Close();
             }
+
+            mycon.Open();
+            using (mycon)
+            {
+                MySqlCommand cmd = new MySqlCommand("SELECT * FROM product", mycon);
+                cmd.ExecuteNonQuery();
+                using (MySqlDataReader mrdr = cmd.ExecuteReader())
+                {
+                    DataTable product = new DataTable();
+                    product.Columns.Add("PID");
+                    product.Columns.Add("PName");
+                    product.Columns.Add("UPrice");
+                    product.Columns.Add("Discount");
+                    while (mrdr.Read())
+                    {
+                        DataRow drow = product.NewRow();
+                        double productPrice = Convert.ToDouble(mrdr["UnitPrice"]);
+                        double discount = productPrice * 0.9;
+                        drow["PID"] = mrdr["ProductId"];
+                        drow["PName"] = mrdr["ProductName"];
+                        drow["UPrice"] = mrdr["UnitPrice"];
+                        drow["Discount"] = discount;
+                        product.Rows.Add(drow);
+                    }
+                    dataGridView1.DataSource = product;
+                }
+            }
+            mycon.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string productid = textBox1.Text;
+            string Name = textBox2.Text;
+            string p = textBox3.Text;
+            Double price = Convert.ToDouble(textBox3.Text);
+
+            MySqlConnection mycon = new MySqlConnection("server=localhost; User Id=root; database=employee");
+            mycon.Open();
+            using (mycon)
+            {
+                MySqlCommand update = new MySqlCommand("UPDATE `product` SET `ProductId`='" + productid + "',`ProductName`='" + Name + "',`UnitPrice`=" + price + " WHERE ProductId='" + productid + "'", mycon);
+                update.ExecuteNonQuery();
+                
+                int result = update.ExecuteNonQuery();
+                if (result < 1)
+                {
+                    System.Windows.Forms.MessageBox.Show("Update failed");
+                }
+                else
+                {
+                    System.Windows.Forms.MessageBox.Show("The Product Name and Price of " + productid + " has been Updated Successfully");
+                }
+                mycon.Close();
+            }
+            mycon.Close();
 
             mycon.Open();
             using (mycon)
