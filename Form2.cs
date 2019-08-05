0b4e977dec6d3a4a0a63490bba896831c054d48e
Anton Jesuthas Nickson Joram <46377224+iamnickson@users.noreply.github.com>
2019-08-05 15:15:19 +0530

    Add files via upload

--- Form2.cs
+++ Form2.cs
@@ -26,33 +26,19 @@ namespace DisplayTable
 
         private void button2_Click(object sender, EventArgs e)
         {
-            String productid = textBox1.Text;
-            //char id = char.Parse(productid);
-            //char[] id = productid.ToCharArray();
-            String Name = textBox2.Text;
-            //char productName = char.Parse(textBox2.Text);
-            //char[] productName = Name.ToCharArray();
-            String p = textBox3.Text;
+            string productid = textBox1.Text;
+            string Name = textBox2.Text;
+            string p = textBox3.Text;
             Double price = Convert.ToDouble(textBox3.Text);
-            /*int i = 0;
-            int a = id.Length;
-            int b = productName.Length;
-            if (a >= b)
-            {
-                i = a;
-            }
-            else
-            {
-                i = b;
-            }
-            */
+
+            System.Windows.Forms.MessageBox.Show(productid);
+            System.Windows.Forms.MessageBox.Show(Name);
+
             MySqlConnection mycon = new MySqlConnection("server=localhost; User Id=root; database=employee");
             mycon.Open();
             using (mycon)
             {
                 MySqlCommand insert = new MySqlCommand("INSERT INTO `product`(`ProductId`, `ProductName`, `UnitPrice`) VALUES ('" + productid + "','" + Name + "'," + price + ")", mycon);
-                //mycon.Open();
-                //insert.ExecuteNonQuery();
                 int result = insert.ExecuteNonQuery();
                 if (result < 1)
                 {
@@ -64,23 +50,12 @@ namespace DisplayTable
                 }
                 mycon.Close();
             }
-            /*
-             //mycon.Open();
-                for (int j = 0; j < i; j++)
-                {
 
-                    MySqlCommand insert = new MySqlCommand("INSERT INTO `product`(`ProductId`, `ProductName`, `UnitPrice`) VALUES ('" + id[j] + "','" + productName[j] + "'," + price + ")", mycon);
-                    
-                    insert.ExecuteNonQuery();
-                }
-             */
             mycon.Open();
             using (mycon)
             {
                 MySqlCommand cmd = new MySqlCommand("SELECT * FROM product", mycon);
-                //mycon.Open();
                 cmd.ExecuteNonQuery();
-
                 using (MySqlDataReader mrdr = cmd.ExecuteReader())
                 {
                     DataTable product = new DataTable();
